1131644593d3b33259f4aee676944c9c7f8f9703
ricardofig016 <ricardifigueiredo016@gmail.com>
2024-03-09 22:07:51 +0000

    create game window

---
+++ game.py
@@ -0,0 +1,17 @@
+import pygame
+import sys
+
+pygame.init()
+
+pygame.display.set_caption("my first platformer")
+screen = pygame.display.set_mode((640, 480))
+
+clock = pygame.time.Clock()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+    pygame.display.update()
+    clock.tick(60)
