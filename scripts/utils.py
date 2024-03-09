6b9b49062a3f33e5e023209b56740b31ea19b3e0
ricardofig016 <ricardifigueiredo016@gmail.com>
2024-03-09 23:49:15 +0000

    load_images function

--- scripts/utils.py
+++ scripts/utils.py
@@ -1,9 +1,16 @@
+import os
 import pygame
 
 BASE_IMG_PATH = "data/images/"
 
 
 def load_image(path):
-    img = pygame.image.load(BASE_IMG_PATH + path).convert()
+    img = pygame.image.load(os.path.join(BASE_IMG_PATH, path)).convert()
     img.set_colorkey((0, 0, 0))
     return img
+
+
+def load_images(path):
+    image_names = os.listdir(os.path.join(BASE_IMG_PATH, path))
+    image_paths = [os.path.join(path, image_name) for image_name in image_names]
+    return [load_image(image_path) for image_path in image_paths]
