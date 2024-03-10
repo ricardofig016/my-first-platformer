d975687734864c6686e805238d9b86a37ad1ccbd
ricardofig016 <ricardifigueiredo016@gmail.com>
2024-03-10 00:55:48 +0000

    tiles_around() and physics_rects_around()

--- classes/tilemap.py
+++ classes/tilemap.py
@@ -1,3 +1,20 @@
+import pygame
+
+
+NEIGHBOR_OFFSETS = [
+    (-1, -1),
+    (-1, 0),
+    (-1, 1),
+    (0, -1),
+    (0, 0),
+    (0, 1),
+    (1, -1),
+    (1, 0),
+    (1, 1),
+]
+PHYSICS_TILES = {"grass", "stone"}
+
+
 class Tilemap:
     def __init__(self, game, tile_size=16) -> None:
         self.game = game
@@ -17,6 +34,33 @@ class Tilemap:
                 "variant": 1,
                 "pos": (10, 5 + i),
             }
+
+    def tiles_around(self, pos):
+        tiles = []
+        tile_location = (int(pos[0] // self.tile_size), int(pos[1] // self.tile_size))
+        for offset in NEIGHBOR_OFFSETS:
+            check_location = (
+                str(tile_location[0] + offset[0])
+                + ";"
+                + str(tile_location[0] + offset[0])
+            )
+            if check_location in self.tilemap:
+                tiles.append(self.tilemap[check_location])
+        return tiles
+
+    def physics_rects_around(self, pos):
+        rects = []
+        for tile in self.tiles_around(pos):
+            if tile in PHYSICS_TILES:
+                rects.append(
+                    pygame.Rect(
+                        tile.pos[0] * self.tile_size,
+                        tile.pos[1] * self.tile_size,
+                        self.tile_size,
+                        self.tile_size,
+                    )
+                )
+        return rects
 
     def render(self, surface):
         for tile in self.offgrid_tiles:
